f5c0fc43b5df0300e95da346d8d4d2ffa54ae86d
Emmanuel Apau <67923993+emmanuel-apau@users.noreply.github.com>
2020-12-11 15:13:46 -0500

    fix typo in dev url replacement (#37)

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ if [ "$1" == "demo" ]; then
   az storage blob upload-batch -s _site/ -d '$web' --account-name usdssimplereportdemo --destination-path '/'
 elif [ "$1" == "dev" ]; then
   echo "Deploying the static landing page to $1"
-  sed -i 's,https://hhs-prime.okta.com/oauth2/default/v1/authorize?client_id=0oa5ahrdfSpxmNZO74h6&amp;response_type=token_id_token&amp;scope=openid&amp;scope=simple_report&amp;redirect_uri=https://simplereport.cdc.gov/app&amp;nonce=thisisnotsafe&amp;state=thisisbogus,http://dev.simplereport.org/app,' _site/index.html
+  sed -i 's,https://hhs-prime.okta.com/oauth2/default/v1/authorize?client_id=0oa5ahrdfSpxmNZO74h6&amp;response_type=token+id_token&amp;scope=openid&amp;scope=simple_report&amp;redirect_uri=https://simplereport.cdc.gov/app&amp;nonce=thisisnotsafe&amp;state=thisisbogus,http://dev.simplereport.org/app,' _site/index.html
   az storage blob upload-batch -s _site/ -d '$web' --account-name simplereportdevapp --destination-path '/'
 elif [ "$1" == "beta" ]; then
   echo "Deploying the static landing page to $1"
