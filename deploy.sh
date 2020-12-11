1d3d34e29d913a8621031e5434a86443923d0922
Emmanuel Apau <67923993+emmanuel-apau@users.noreply.github.com>
2020-12-11 11:06:27 -0500

    Setup deploy to azure on merge to main

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,17 @@ git pull
 npm install
 gem install bundler
 bundle install
+npm run setup
 npm run build
 
 if [ "$1" == "demo" ]; then
   echo "Deploying the static landing page to $1"
   sed -i 's,https://hhs-prime.okta.com/oauth2/default/v1/authorize?client_id=0oa5ahrdfSpxmNZO74h6&response_type=token&scope=openid&scope=simple_report&redirect_uri=https://simplereport.cdc.gov/app&nonce=thisisnotsafe&state=thisisbogus,https://demo.simplereport.org/app,' _site/index.html
   az storage blob upload-batch -s _site/ -d '$web' --account-name usdssimplereportdemo --destination-path '/'
+elif [ "$1" == "dev" ]; then
+  echo "Deploying the static landing page to $1"
+  sed -i 's,https://hhs-prime.okta.com/oauth2/default/v1/authorize?client_id=0oa5ahrdfSpxmNZO74h6&amp;response_type=token&amp;scope=openid&amp;scope=simple_report&amp;redirect_uri=https://simplereport.cdc.gov/app&amp;nonce=thisisnotsafe&amp;state=thisisbogus,http://dev.simplereport.org/app,' _site/index.html
+  az storage blob upload-batch -s _site/ -d '$web' --account-name simplereportdevapp --destination-path '/'
 elif [ "$1" == "beta" ]; then
   echo "Deploying the static landing page to $1"
   az storage blob upload-batch -s _site/ -d '$web' --account-name usdssimplereportbeta --destination-path '/'
