00d1e0daef7493275e91511b6f262a8269537a8b
Emmanuel Apau <67923993+emmanuel-apau@users.noreply.github.com>
2020-12-28 10:31:10 -0500

    stg deployment

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,10 @@ elif [ "$1" == "test" ]; then
   echo "Deploying the static landing page to $1"
   sed -i 's,https://hhs-prime.okta.com/oauth2/default/v1/authorize?client_id=0oa5ahrdfSpxmNZO74h6&amp;response_type=token+id_token&amp;scope=openid&amp;scope=simple_report&amp;redirect_uri=https://simplereport.cdc.gov/app&amp;nonce=thisisnotsafe&amp;state=thisisbogus,https://test.simplereport.gov/app,' _site/index.html
   az storage blob upload-batch -s _site/ -d '$web' --account-name simplereporttestapp --destination-path '/'
+elif [ "$1" == "stg" ]; then
+  echo "Deploying the static landing page to $1"
+  sed -i 's,https://hhs-prime.okta.com/oauth2/default/v1/authorize?client_id=0oa5ahrdfSpxmNZO74h6&amp;response_type=token+id_token&amp;scope=openid&amp;scope=simple_report&amp;redirect_uri=https://simplereport.cdc.gov/app&amp;nonce=thisisnotsafe&amp;state=thisisbogus,https://stg.simplereport.gov/app,' _site/index.html
+  az storage blob upload-batch -s _site/ -d '$web' --account-name simplereportstgapp --destination-path '/'
 elif [ "$1" == "beta" ]; then
   echo "Deploying the static landing page to $1"
   az storage blob upload-batch -s _site/ -d '$web' --account-name usdssimplereportbeta --destination-path '/'
