dd99e2906a6ae7ad8a9ff54276b91442685c9f34
Ben Warfield <ben.warfield@usds.dhs.gov>
2021-01-07 22:53:10 -0500

    Cleaned up deploy.sh using case and variable interpolation.

--- deploy.sh
+++ deploy.sh
@@ -7,19 +7,13 @@ bundle install
 npm run setup
 npm run build:$1
 
-if [ "$1" == "demo" ]; then
-  echo "Deploying the static landing page to $1"
-  az storage blob upload-batch -s _site/ -d '$web' --account-name simplereportdemoapp --destination-path '/'
-elif [ "$1" == "dev" ]; then
-  echo "Deploying the static landing page to $1"
-  az storage blob upload-batch -s _site/ -d '$web' --account-name simplereportdevapp --destination-path '/'
-elif [ "$1" == "stg" ]; then
-  echo "Deploying the static landing page to $1"
-  az storage blob upload-batch -s _site/ -d '$web' --account-name simplereportstgapp --destination-path '/'
-elif [ "$1" == "test" ]; then
-  echo "Deploying the static landing page to $1"
-  az storage blob upload-batch -s _site/ -d '$web' --account-name simplereporttestapp --destination-path '/'
-elif [ "$1" == "beta" ]; then
-  echo "Deploying the static landing page to $1"
-  az storage blob upload-batch -s _site/ -d '$web' --account-name usdssimplereportbeta --destination-path '/'
-fi
+case "$1" in
+  demo|dev|stg|test)
+    echo "Deploying the static landing page to $1"
+    az storage blob upload-batch -s _site/ -d '$web' --account-name simplereport${1}app --destination-path '/'
+    ;;
+  beta)
+    echo "Deploying the static landing page to $1"
+    az storage blob upload-batch -s _site/ -d '$web' --account-name usdssimplereportbeta --destination-path '/'
+    ;;
+esac
