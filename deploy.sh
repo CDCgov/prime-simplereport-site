00feff32ccf389b325260e106f09fd3ced2633a5
Emmanuel Apau <67923993+emmanuel-apau@users.noreply.github.com>
2021-01-14 21:11:32 -0500

    SimpleReport.gov Prod deployment config (#51)
    * added prod deploy & fixed urls in env configs
    
    * missing scripts

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ npm run setup
 npm run build:$1
 
 case "$1" in
-  demo|dev|stg|test)
+  demo|dev|stg|test|prod)
     echo "Deploying the static landing page to $1"
     az storage blob upload-batch -s _site/ -d '$web' --account-name simplereport${1}app --destination-path '/'
     ;;
