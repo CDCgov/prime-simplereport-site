c90b4a2f21dc54f98cf5724e27780cf54bb0484b
Emmanuel Apau <67923993+emmanuel-apau@users.noreply.github.com>
2021-01-29 10:59:21 -0500

    added pentest deployment config

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ npm run setup
 npm run build:$1
 
 case "$1" in
-  demo|dev|stg|test|prod)
+  demo|dev|stg|test|prod|pentest)
     echo "Deploying the static landing page to $1"
     az storage blob upload-batch -s _site/ -d '$web' --account-name simplereport${1}app --destination-path '/'
     ;;
