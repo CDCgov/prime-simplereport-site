82cb49054bb8e45938bdea4b6840f3e6ff1fe062
elisa lee <emyl3@users.noreply.github.com>
2022-08-23 14:34:39 -0500

    Revert "Trap focus when touchpoints modal is open (#345)" (#351)
    This reverts commit ff85b0e1764d4e8916274191a5d2adf000bed8a0.

--- assets/js/touchpoints.js
+++ assets/js/touchpoints.js
@@ -1,6 +1,5 @@
 /**
  * Script taken from our touchpoints configuration here: https://touchpoints.app.cloud.gov/touchpoints/2a6e9509.js
- * NOTE: When updating script ensure sr-touchpoints.js works as intended
  */
 
 // Form components are namespaced under 'fba' = 'Feedback Analytics'
