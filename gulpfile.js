d104996d6af0abcaaafc77fc2753ba20b852f1db
Bob Zhao <bob@skylight.digital>
2022-10-06 16:03:33 -0500

    update sass versioning and add styling to text link

--- gulpfile.js
+++ gulpfile.js
@@ -11,18 +11,18 @@ USWDS SASS GULPFILE
 ----------------------------------------
 */
 
-const autoprefixer = require('autoprefixer');
-const csso = require('postcss-csso');
-const uncss = require('postcss-uncss');
-const gulp = require('gulp');
-const pkg = require('./node_modules/uswds/package.json');
-const postcss = require('gulp-postcss');
-const replace = require('gulp-replace');
-const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
-const uswds = require('./node_modules/uswds-gulp/config/uswds');
+const autoprefixer = require("autoprefixer");
+const csso = require("postcss-csso");
+const uncss = require("postcss-uncss");
+const gulp = require("gulp");
+const pkg = require("./node_modules/uswds/package.json");
+const postcss = require("gulp-postcss");
+const replace = require("gulp-replace");
+const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
+const uswds = require("./node_modules/uswds-gulp/config/uswds");
 
-sass.compiler = require('sass');
+sass.compiler = require("sass");
 
 /*
 ----------------------------------------
@@ -36,27 +36,27 @@ PATHS
 */
 
 // Project Sass source directory
-const PROJECT_SASS_SRC = './_sass';
+const PROJECT_SASS_SRC = "./_sass";
 
 // Images destination
-const IMG_DEST = './assets/uswds/img';
+const IMG_DEST = "./assets/uswds/img";
 
 // Fonts destination
-const FONTS_DEST = './assets/uswds/fonts';
+const FONTS_DEST = "./assets/uswds/fonts";
 
 // Javascript destination
-const JS_DEST = './assets/uswds/js';
+const JS_DEST = "./assets/uswds/js";
 
 // Compiled CSS destination
-const CSS_DEST = './assets/uswds/css';
+const CSS_DEST = "./assets/uswds/css";
 
 // Site CSS destination
 // Like the _site/assets/css directory in Jekyll, if necessary.
 // If using, uncomment line 106
-const SITE_CSS_DEST = './assets/uswds/css';
+const SITE_CSS_DEST = "./assets/uswds/css";
 
 // Check if building for GH pages
-const ghPages = process.argv[3] === '--ghpages' ? true : false;
+const ghPages = process.argv[3] === "--ghpages" ? true : false;
 
 /*
 ----------------------------------------
@@ -64,31 +64,31 @@ TASKS
 ----------------------------------------
 */
 
-gulp.task('copy-uswds-setup', () => {
+gulp.task("copy-uswds-setup", () => {
   return gulp
     .src(`${uswds}/scss/theme/**/**`)
     .pipe(gulp.dest(`${PROJECT_SASS_SRC}`));
 });
 
-gulp.task('copy-uswds-fonts', () => {
+gulp.task("copy-uswds-fonts", () => {
   return gulp.src(`${uswds}/fonts/**/**`).pipe(gulp.dest(`${FONTS_DEST}`));
 });
 
-gulp.task('copy-subset-fonts', () => {
+gulp.task("copy-subset-fonts", () => {
   return gulp
     .src(`_subsetfonts/**/**`)
     .pipe(gulp.dest(`${FONTS_DEST}`, { overwrite: true }));
 });
 
-gulp.task('copy-uswds-images', () => {
+gulp.task("copy-uswds-images", () => {
   return gulp.src(`${uswds}/img/**/**`).pipe(gulp.dest(`${IMG_DEST}`));
 });
 
-gulp.task('copy-uswds-js', () => {
+gulp.task("copy-uswds-js", () => {
   return gulp.src(`${uswds}/js/**/**`).pipe(gulp.dest(`${JS_DEST}`));
 });
 
-gulp.task('build-sass', function () {
+gulp.task("build-sass", function () {
   var plugins = [
     // Autoprefix
     autoprefixer({
@@ -111,27 +111,27 @@ gulp.task('build-sass', function () {
           ],
         })
       )
-      .pipe(replace(/\buswds @version\b/g, 'based on uswds v' + pkg.version))
+      .pipe(replace(/\buswds @version\b/g, "based on uswds v" + pkg.version))
       .pipe(
         ghPages
-          ? replace(/\/assets\/img/g, '/prime-simplereport-site/assets/img')
-          : replace(/this will never be found/g, 'this will never be found')
+          ? replace(/\/assets\/img/g, "/prime-simplereport-site/assets/img")
+          : replace(/this will never be found/g, "this will never be found")
       )
       .pipe(postcss(plugins))
-      .pipe(sourcemaps.write('.'))
+      .pipe(sourcemaps.write("."))
       // uncomment the next line if necessary for Jekyll to build properly
       .pipe(gulp.dest(`${SITE_CSS_DEST}`))
       .pipe(gulp.dest(`${CSS_DEST}`))
   );
 });
 
-gulp.task('uncss', function () {
+gulp.task("uncss", function () {
   var plugins = [
     uncss({
-      html: ['./_site/**/*.html'],
-      htmlroot: './_site',
+      html: ["./_site/**/*.html"],
+      htmlroot: "./_site",
       ignore: [
-        '.usa-overlay.is-visible',
+        ".usa-overlay.is-visible",
         /^\.usa-nav/,
         /^\.usa-accordion__button/,
         /^\.usa-banner__button/,
@@ -139,9 +139,9 @@ gulp.task('uncss', function () {
         /^\.usa-alert/,
         /^\.fba-alert/,
         /^\.usa-menu-btn/,
-        '.page--home .usa-nav__primary button[aria-expanded=true]',
-        '#fba-touchpoints-page-form-yes',
-        '#fba-touchpoints-page-form-no',
+        ".page--home .usa-nav__primary button[aria-expanded=true]",
+        "#fba-touchpoints-page-form-yes",
+        "#fba-touchpoints-page-form-no",
       ],
     }),
   ];
@@ -152,21 +152,21 @@ gulp.task('uncss', function () {
 });
 
 gulp.task(
-  'init',
+  "init",
   gulp.series(
-    'copy-uswds-setup',
-    'copy-uswds-fonts',
-    'copy-subset-fonts',
-    'copy-uswds-images',
-    'copy-uswds-js',
-    'build-sass'
+    "copy-uswds-setup",
+    "copy-uswds-fonts",
+    "copy-subset-fonts",
+    "copy-uswds-images",
+    "copy-uswds-js",
+    "build-sass"
   )
 );
 
-gulp.task('watch-sass', function () {
-  gulp.watch(`${PROJECT_SASS_SRC}/**/*.scss`, gulp.series('build-sass'));
+gulp.task("watch-sass", function () {
+  gulp.watch(`${PROJECT_SASS_SRC}/**/*.scss`, gulp.series("build-sass"));
 });
 
-gulp.task('watch', gulp.series('build-sass', 'watch-sass'));
+gulp.task("watch", gulp.series("build-sass", "watch-sass"));
 
-gulp.task('default', gulp.series('watch'));
+gulp.task("default", gulp.series("watch"));
