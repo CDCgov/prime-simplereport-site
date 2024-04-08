d362e5f5cb4493421ce127b8a530a9849b10436a
Bob Zhao <bob@skylight.digital>
2024-04-08 13:56:05 -0400

    try ignore glob matching

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,23 @@
 import frontmatter from "eslint-plugin-frontmatter"
+import globals from "globals";
 
 export default [
     {
-        browser: true,
-        commonjs: true,
-        es2020: true,
-        extends: {
-            "eslint" : "recommended"
+        files:["assets/**/*.js"],
+        ignores: [
+            "**/uswds.js",
+            "**/uswds.min.js",
+            "**/main.js"
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            sourceType: "module",
+            globals: {
+                ...globals.browser,
+            }
         },
-        files: ["assets/js/"],
         plugins: {
             frontmatter: frontmatter
         },
-        ignores: [
-            "uswds.min.js",
-            "uswds-init*.js",
-            "gtm.js",
-            "main.js",
-            "uswds.js",
-            "jquery3.5.1.min.js",
-            "url-search-param-polyfil.js",
-            "anchor.min.js",
-            "touchpoints.js"
-        ]
     }
 ];
